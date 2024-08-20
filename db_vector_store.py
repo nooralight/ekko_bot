a83e57ebc0df90e3cfadd9b062593dd6b8bffd17
noorlight007 <2017100000041@seu.edu.bd>
2024-08-21 01:59:40 +0600

    update

--- db_vector_store.py
+++ db_vector_store.py
@@ -50,79 +50,79 @@ def update_files(file_arr):
 
 file_arr = [{
         'file-gWOn52MgfrnPgPzx9wihMznS': {
-          "file_url": 'http://159.65.152.150:3000/static/training_files/ml3AJ_aligning-values-goals-2024-nick-regan-cpcc-lhaje.html',
+          "file_url": 'http://78.142.47.22:3000/static/training_files/ml3AJ_aligning-values-goals-2024-nick-regan-cpcc-lhaje.html',
           "file_name": 'aligning-values-goals-2024-nick-regan-cpcc-lhaje.html'
         }
       },
       {
         'file-mWkKanjFNFZpwVKiCpjroIaa': {
-          "file_url": 'http://159.65.152.150:3000/static/training_files/xeImD_buzz-challenge-nick-regan.html',
+          "file_url": 'http://78.142.47.22:3000/static/training_files/xeImD_buzz-challenge-nick-regan.html',
           "file_name": 'buzz-challenge-nick-regan.html'
         }
       },
       {
         'file-6ILygpiyBwTXGnk79Hh2eBrh': {
-          "file_url": 'http://159.65.152.150:3000/static/training_files/w6mHE_creating-philosophy-stance-example-team-coaching-nick-regan-cpcc.html',
+          "file_url": 'http://78.142.47.22:3000/static/training_files/w6mHE_creating-philosophy-stance-example-team-coaching-nick-regan-cpcc.html',
           "file_name": 'creating-philosophy-stance-example-team-coaching-nick-regan-cpcc.html'
         }
       },
       {
         'file-RmroJwdpDqzwNIHZhxXsa9BS': {
-          "file_url": 'http://159.65.152.150:3000/static/training_files/ZBRDZ_feeling-alive-teams-nick-regan.html',
+          "file_url": 'http://78.142.47.22:3000/static/training_files/ZBRDZ_feeling-alive-teams-nick-regan.html',
           "file_name": 'feeling-alive-teams-nick-regan.html'
         }
       },
       {
         'file-5CJxedFu7AXD7dR4AXIULZuh': {
-          "file_url": 'http://159.65.152.150:3000/static/training_files/3V6VD_future-ai-human-ness-leaders-teams-nick-regan-cpcc.html',
+          "file_url": 'http://78.142.47.22:3000/static/training_files/3V6VD_future-ai-human-ness-leaders-teams-nick-regan-cpcc.html',
           "file_name": 'future-ai-human-ness-leaders-teams-nick-regan-cpcc.html'
         }
       },
       {
         'file-kNrxzeKwi8d1Lj58Pagcjj5N': {
-          "file_url": 'http://159.65.152.150:3000/static/training_files/fDLMj_how-measure-magic-generated-effective-teams-nick-regan.html',
+          "file_url": 'http://78.142.47.22:3000/static/training_files/fDLMj_how-measure-magic-generated-effective-teams-nick-regan.html',
           "file_name": 'how-measure-magic-generated-effective-teams-nick-regan.html'
         }
       },
       {
         'file-PnXT8a2vLQcTUEXa6dNbtgWv': {
-          "file_url": 'http://159.65.152.150:3000/static/training_files/SSzoH_joy-receivingan-unexpected-gift-from-your-team-mates-nick-regan.html',
+          "file_url": 'http://78.142.47.22:3000/static/training_files/SSzoH_joy-receivingan-unexpected-gift-from-your-team-mates-nick-regan.html',
           "file_name": 'joy-receivingan-unexpected-gift-from-your-team-mates-nick-regan.html'
         }
       },
       {
         'file-gULLA2IziByLBt1jjYMJuTfB': {
-          "file_url": 'http://159.65.152.150:3000/static/training_files/NrZR7_time-battle-our-nick-regan-cpcc-c2bec.html',
+          "file_url": 'http://78.142.47.22:3000/static/training_files/NrZR7_time-battle-our-nick-regan-cpcc-c2bec.html',
           "file_name": 'time-battle-our-nick-regan-cpcc-c2bec.html'
         }
       },
       {
         'file-c6V59ZoxUXitObZSkpvh0sA0': {
-          "file_url": 'http://159.65.152.150:3000/static/training_files/Vl6o8_upskilling-our-creativity-lets-experiment-learn-nick-regan-cpcc.html',
+          "file_url": 'http://78.142.47.22:3000/static/training_files/Vl6o8_upskilling-our-creativity-lets-experiment-learn-nick-regan-cpcc.html',
           "file_name": 'upskilling-our-creativity-lets-experiment-learn-nick-regan-cpcc.html'
         }
       },
       {
         'file-NvWSH4Tf7LGdlUMWtFkXHNM4': {
-          "file_url": 'http://159.65.152.150:3000/static/training_files/LqSDM_what-your-favorite-metaphors-nick-regan.html',
+          "file_url": 'http://78.142.47.22:3000/static/training_files/LqSDM_what-your-favorite-metaphors-nick-regan.html',
           "file_name": 'what-your-favorite-metaphors-nick-regan.html'
         }
       },
       {
         'file-rpopMVQuS5rJGZHp2LFtBGyj': {
-          "file_url": 'http://159.65.152.150:3000/static/training_files/zUTg1_when-can-i-call-myself-coach-nick-regan.html',
+          "file_url": 'http://78.142.47.22:3000/static/training_files/zUTg1_when-can-i-call-myself-coach-nick-regan.html',
           "file_name": 'when-can-i-call-myself-coach-nick-regan.html'
         }
       },
       {
         'file-ot6MrcL6BxHYql61QoNLEQti': {
-          "file_url": 'http://159.65.152.150:3000/static/training_files/ewurV_who-am-i-you-nick-regan.html',
+          "file_url": 'http://78.142.47.22:3000/static/training_files/ewurV_who-am-i-you-nick-regan.html',
           "file_name": 'who-am-i-you-nick-regan.html'
         }
       },
       {
         'file-4gkz5Z06Sx12kTJxfYymZePo': {
-          "file_url": 'http://159.65.152.150:3000/static/training_files/VRRu7_Nick Regan LinkedIn Posts good.pdf',
+          "file_url": 'http://78.142.47.22:3000/static/training_files/VRRu7_Nick Regan LinkedIn Posts good.pdf',
           "file_name": 'Nick Regan LinkedIn Posts good.pdf'
         }
       }
